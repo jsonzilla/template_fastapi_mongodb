3a3f490fb58a68efc37bc47d70be674e680e17cb
Jeison C <j@jsonzilla.com>
2023-05-04 18:06:15 -0300

    fix

--- tests/repositories/test_base_repository.py
+++ tests/repositories/test_base_repository.py
@@ -96,7 +96,7 @@ async def test_get_all(db):
 
 async def test_update(db):
     valid_json['_id'] = objectid.ObjectId().__str__()  # type: ignore
-    obj: dict = await repo.create(valid_json, db)
+    obj = await repo.create(valid_json, db)
     obj['name'] = 'test2'
     await repo.update(obj['_id'], obj, db)
     assert obj == (await repo.get_by_id(obj['_id'], db))
@@ -115,7 +115,7 @@ async def test_update_list(db):
 
 
 async def test_delete(db):
-    valid_json['_id'] = objectid.ObjectId().__str__()  # type: ignore
+    valid_json['_id'] = objectid.ObjectId().__str__()
     obj = await repo.create(valid_json, db)
     await repo.delete(obj['_id'], db)
     assert (await repo.get_by_id(obj['_id'], db)) is None
